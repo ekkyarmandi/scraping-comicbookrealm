dddacf5b9e7be58aab4876b32e3c7a2d034a19e0
ekkyarmandi <ekkyarmandi@gmail.com>
2022-12-31 12:15:55 +0700

    refined scraping extra info process time

--- app_v2.py
+++ app_v2.py
@@ -14,146 +14,52 @@ def check_folder(path):
         os.mkdir(path)
 
 
-class ComicPlaywright:
+class Comic:
+    detail = {}
 
-    series_id = None
-    issues_dir = "series"
-
-    def __init__(self, url=None):
-        self.url = (
-            "https://comicbookrealm.com/series/113/0/marvel-comics-amazing-spider-man-vol-1"
-            if not url
-            else url
-        )
-        self.issues = []
-        self.open_browser()
-        self.get_series_id()
-
-    def open_browser(self):
-        # pw.start(), pw.stop(), browser.new_page(), browser.close()
-        print("Open the browser", end=" ")
-        self.pw = sync_playwright().start()
-        self.browser = self.pw.chromium.launch()
-        print("[green]OK[/green]")
+    def __init__(self, id="#", series_id="#", url=None, output="output", page=None):
+        self.id = id
+        self.url = url
+        self.page = page
+        self.output = output
+        self.series_id = series_id
+        self.filename = self.create_filename()
 
     def urljoin(self, path):
         parse = urlparse(self.url)
         base_url = f"{parse.scheme}://{parse.netloc}"
         return urljoin(base_url, path)
 
-    def create_file(self, item):
-        dst_path = os.path.join("output", self.series_id)
-        if not os.path.exists(dst_path):
-            os.makedirs(dst_path)
-        return os.path.join(dst_path, item["id"] + ".json")
+    def get(self):
+        """Get comic detail"""
 
-    def get_series_id(self):
+        print("Get content detail for #" + str(self.id), end=" ")
 
-        # Get series id
-        print("Get the list of issues", end=" ")
-        match = re.search(r"series/(\d+)/", self.url)
-        if match:
-            self.series_id = match.group(1)
+        # Create filename and check issue data existance
+        if not os.path.exists(self.filename):
 
-        # Check the jsonfile existance
-        check_folder(self.issues_dir)
-        jsonfile = os.path.join(self.issues_dir, str(self.series_id) + ".json")
-        if not os.path.exists(jsonfile):
-            self.issues = self.get_issues()
-            json.dump(self.issues, open(jsonfile, "w"), indent=4)
-        else:
-            self.issues = json.load(open(jsonfile))
-        print("[green]OK[/green]")
+            # Start
+            start = time.time()
 
-    def get_issues(self):
-
-        # Define the page variable
-        issues = []
-        current_page = 1
-        page_length = None
-
-        # Open the browser
-        page = self.browser.new_page()
-
-        # Make a get request to the url
-        page.goto(self.url)
-
-        # Wait till issues table exists
-        # try:
-
-        # While not the end of the issues page
-        while not page_length or current_page <= page_length:
-            css_selector = (
-                f"div#series-details div[class=page_{current_page}] table"
-            )
-            page.is_visible(css_selector, timeout=10000)
-            html = page.inner_html("div#series-details")
-            soup = BeautifulSoup(html, "html.parser")
-
-            # Get the issue page and Find the issue page length
-            if not page_length:
-                try:
-                    page_length = int(soup.select("tr.type_footer a[class=g]")[-1].text)
-                except IndexError:
-                    page_length = 1
-
-            # Collect the rows
-            item_list = soup.select(
-                f"div[class=page_{current_page}] table tr[class*=comic]"
-            )
-            for r in item_list:
-                a = r.select_one("td span + a")
-                issue = {
-                    "id": r.get("id", "#"),
-                    "link": self.urljoin(a.get("href")),
-                }
-                issues.append(issue)
-
-            # Find the issues table pagination tab and click next
-            if current_page < page_length:
-                pagination = page.query_selector_all("tr.type_footer a.g")
-                next_page = pagination[current_page]
-                next_page.click()
-                time.sleep(2)
-
-            # Add current page state by one
-            current_page += 1
-
-        # except Exception as E:
-        #     print(f"[red]ERROR {E}[/red]")
-
-        # close browser and stop playwright
-        self.browser.close()
-        self.pw.stop()
-
-        return issues
-
-    def get_detail(self, issue):
-
-        start = time.time()
-        comic_id = issue.get("id")
-        print("Get content detail from #" + comic_id, end=" ")
-
-        try:
-
-            # Get issue details
+            # Get details
             detail = {}
-            page = self.browser.new_page()
-            page.goto(issue.get("link"))
-            page.is_visible("#details", timeout=10000)
-            html = BeautifulSoup(page.inner_html("#details"), "html.parser")
+            self.page.goto(self.url)
+            self.page.wait_for_selector("#details", timeout=10000)
 
-            # Get issue title, vol, and years
-            soup = BeautifulSoup(page.inner_html("#series-information"), "html.parser")
+            ## Get issue title, vol, and years
+            soup = BeautifulSoup(
+                self.page.inner_html("#series-information"), "html.parser"
+            )
             detail = {
-                "Series": page.inner_text("#series_comic_details_page h2").strip(),
-                "Publisher": page.inner_text("#series-information a").strip(),
+                "Series": self.page.inner_text("#series_comic_details_page h2").strip(),
+                "Publisher": self.page.inner_text("#series-information a").strip(),
                 "Volume/Years": list(soup.strings)[0].strip(),
             }
 
-            # Get content detail
+            ## Get the detail detail
+            html = BeautifulSoup(self.page.inner_html("#details"), "html.parser")
             item_list = html.select("tr")
-            for item in item_list:
+            for i, item in enumerate(item_list):
                 if i == 0:
                     label = (
                         list(item.select_one(".label").strings)[-1]
@@ -164,45 +70,61 @@ class ComicPlaywright:
                     label = item.select_one(".label").text.replace(":", "").strip()
                 detail.update({label: item.select_one(".data").text.strip()})
 
+            # Get extra info
             tabs = ["contributors", "characters", "collects", "events", "history"]
-            for a in tabs:
-                php_url = (
-                    f"https://comicbookrealm.com/comic.php?a={a}_tab&comic={comic_id}"
-                )
-                page.goto(php_url)
-                page.is_visible("body", timeout=10000)
-                html = BeautifulSoup(page.inner_html("body"), "html.parser")
+            for tab in tabs:
 
-                detail.update({a.title(): []})
+                # Prepare the key for detail dictionary
+                key = tab.title()
+                detail.update({key: []})
 
-                # Get contributors
-                if a == "contributors":
-                    detail[a.title()] = self.get_contributors(html.select("ul > li"))
+                # Click the tab
+                selector = f"ul.tabs2 li[ref={tab}]"
+                self.page.click(selector)
 
-                elif a == "characters":
-                    detail[a.title()] = self.get_characters(html.select("ul > li"))
+                # Wait till the tab ul tag is exists
+                try:
+                    self.page.wait_for_selector(
+                        f"#comic-extra-info #{tab} ul", timeout=3000
+                    )
 
-                elif a == "collects":
-                    text = page.inner_text("body")
-                    if text != "":
-                        html = BeautifulSoup(page.content(), "html.parser")
-                        detail[a.title()] = self.get_collects(
-                            html.select("body > ul > li")
+                    html = BeautifulSoup(self.page.inner_html(f"#{tab}"), "html.parser")
+                    if tab == "contributors":
+                        detail[key] = self.get_contributors(html.select("ul > li"))
+
+                    elif tab == "characters":
+                        detail[key] = self.get_characters(html.select("ul > li"))
+
+                    elif tab == "collects":
+                        html = BeautifulSoup(
+                            page.inner_html("#comic-extra-info"), "html.parser"
+                        )
+                        detail[key] = self.get_collects(
+                            html.select(f"div#{tab} > ul > li")
                         )
 
-                elif a == "events":
-                    detail[a.title()] = self.get_events(html.select("ul > li"))
+                    elif tab == "events":
+                        detail[key] = self.get_events(html.select("ul > li"))
 
-                elif a == "history":
-                    html = BeautifulSoup(page.content(), "html.parser")
-                    detail[a.title()] = self.get_history(html.select("body > ul > li"))
+                    elif tab == "history":
+                        html = BeautifulSoup(
+                            page.inner_html("#comic-extra-info"), "html.parser"
+                        )
+                        detail[key] = self.get_history(
+                            html.select(f"div#{tab} > ul > li")
+                        )
 
-            end = time.time()
-            print(f"[green]OK {end-start:.2f} sec[/green]")
-            return detail
+                except TimeoutError:
+                    print(tab, "tab is empty")
 
-        except Exception as E:
-            print(f"[red]ERROR at [b]get_detail[/b]: {E}[/red]")
+            # Save content as JSON file
+            self.detail = detail
+            self.save_as_json()
+
+            # End
+            print(f"[green]OK {time.time()-start:.2f} sec[/green]")
+        else:
+            print(f"[yellow]SKIPPED[/yellow]")
 
     def get_contributors(self, item_list):
         contributors = []
@@ -289,18 +211,139 @@ class ComicPlaywright:
             )
         return histories
 
+    def create_filename(self, ext="json"):
+        """Create filename"""
+        name = ".".join([self.id, ext])
+        dst_path = os.path.join(self.output, self.series_id)
+        check_folder(dst_path)
+        return os.path.join(dst_path, name)
+
+    def save_as_json(self):
+        """Write it out as JSON file"""
+
+        json.dump(self.detail, open(self.filename, "w"), indent=4)
+
+
+class ComicBookRealm:
+
+    series_id = None
+    series_dir = "series"
+    dst_path = "output"
+
+    def __init__(self, page=None):
+        self.page = page
+
+        # check folder existance
+        check_folder(self.series_dir)
+        check_folder(self.dst_path)
+
+    def urljoin(self, path):
+        parse = urlparse(self.url)
+        base_url = f"{parse.scheme}://{parse.netloc}"
+        return urljoin(base_url, path)
+
+    def get_issues(self, series):
+
+        # Start
+        start = time.time()
+        series_id = str(series["id"])
+        print(f"Get series list of issues #" + series_id, end=" ")
+
+        # Parepare the json file for the output
+        jsonfile = os.path.join(self.series_dir, series_id + ".json")
+        if not os.path.exists(jsonfile):
+            color = "green"
+            issues = self.collect_issues(series)
+            json.dump(issues, open(jsonfile, "w"), indent=4)
+        else:
+            color = "blue"
+            issues = json.load(open(jsonfile))
+
+        # End
+        print(f"[{color}]OK {time.time()-start:.2f} sec[/{color}]")
+        return issues
+
+    def collect_issues(self, series):
+
+        # Define the page variable
+        issues = []
+        current_page = 1
+        page_length = None
+
+        self.url = series["url"]
+        self.series_id = series["id"]
+
+        # Make a get request to the url
+        self.page.goto(series["url"], timeout=60 * 1e3, wait_until="domcontentloaded")
+
+        # While not the end of the issues page
+        while not page_length or current_page <= page_length:
+            css_selector = f"div#series-details div[class=page_{current_page}] table"
+            self.page.wait_for_selector(css_selector, timeout=10000)
+            soup = BeautifulSoup(
+                self.page.inner_html("div#series-details"), "html.parser"
+            )
+
+            # Get the issue page and Find the issue page length
+            if not page_length:
+                try:
+                    page_length = int(soup.select("tr.type_footer a[class=g]")[-1].text)
+                except IndexError:
+                    page_length = 1
+
+            # Collect the rows
+            item_list = soup.select(
+                f"div[class=page_{current_page}] table tr[class*=comic]"
+            )
+            for r in item_list:
+                a = r.select_one("td span + a")
+                issue = {
+                    "id": r.get("id", "#"),
+                    "link": self.urljoin(a.get("href")),
+                }
+                issues.append(issue)
+
+            # Find the issues table pagination tab and click next
+            if current_page < page_length:
+                pagination = self.page.query_selector_all("tr.type_footer a.g")
+                next_page = pagination[current_page]
+                next_page.click()
+                time.sleep(2)
+
+            # Add current page state by one
+            current_page += 1
+        return issues
+
 
 if __name__ == "__main__":
 
+    # Load series file
     series = json.load(open("marvel-comic.json"))
-    for s in series:
-        n = s.get("number_of_issues")
-        print(
-            f"[green]Start scraping series: {s.get('url')} ({s.get('years')})[/green]"
-        )
-        cbr = ComicPlaywright(url=s.get("url"))
-        for i in cbr.issues:
-            jsonfile = cbr.create_file(i)
-            if not os.path.exists(jsonfile):
-                content = cbr.get_detail(i)
-                json.dump(content, open(jsonfile, "w"), indent=4)
+
+    # Open the playwright and the page
+    with sync_playwright() as pw:
+        with pw.chromium.launch() as browser:
+            with browser.new_page() as page:
+
+                # Assign the page to comic obejct
+                cbr = ComicBookRealm(page=page)
+
+                # Iterate the series
+                for s in series:
+                    print(
+                        f"\n[green]Start scraping series: {s['url']} ({s['years']})[/green]"
+                    )
+
+                    # Get the issues list
+                    issues = cbr.get_issues(series=s)
+
+                    # Get issue detail then save it as jsonfile
+                    for i in issues:
+                        comic = Comic(
+                            id=i["id"],
+                            series_id=s["id"],
+                            url=i["link"],
+                            page=page,
+                            output=cbr.dst_path,
+                        )
+                        comic.get()
